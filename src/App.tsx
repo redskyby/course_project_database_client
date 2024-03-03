6fb3f822503c4611b7ac0bc69adbbe914ef4df4f
Docenko Pasha <pashadocenko@gmail.com>
2024-03-03 23:14:06 +0300

    Tabs were added. (works)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./App.css";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Tabs defaultActiveKey="profile" id="justify-tab-example" className="mb-3" justify>
+            <Tab eventKey="home" title="Home">
+                Tab content for Home
+            </Tab>
+            <Tab eventKey="profile" title="Profile">
+                Tab content for Profile
+            </Tab>
+            <Tab eventKey="longer-tab" title="Loooonger Tab">
+                Tab content for Loooonger Tab
+            </Tab>
+            <Tab eventKey="contact" title="Contact" disabled>
+                Tab content for Contact
+            </Tab>
+        </Tabs>
     );
 }
 
