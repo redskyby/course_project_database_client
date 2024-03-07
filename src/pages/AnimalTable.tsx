c107bac6986a7fe9449748f0bd05722ec4c0e276
Docenko Pasha <pashadocenko@gmail.com>
2024-03-07 23:21:55 +0300

    AnimalTable.tsx was started to init.

--- src/pages/AnimalTable.tsx
+++ src/pages/AnimalTable.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
 
 const AnimalTable = () => {
     return (
         <Container>
-            <div>hello</div>
+            <Table striped bordered hover responsive>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
         </Container>
     );
 };
