0e8eb3fc04ab98c43f025e7ec08b09480ee92abb
Docenko Pasha <pashadocenko@gmail.com>
2024-03-16 00:15:18 +0300

    App.module.scss was deleted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import style from "./App.module.scss";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AppRoutes from "./components/AppRoutes";
