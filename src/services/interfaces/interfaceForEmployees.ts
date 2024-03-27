f2c7bb17c741d4eb1c21d49c48c24bb31ccc1c20
Docenko Pasha <pashadocenko@gmail.com>
2024-03-27 22:29:17 +0300

    interfaceForEmployees.ts was added.

---
+++ src/services/interfaces/interfaceForEmployees.ts
@@ -0,0 +1,9 @@
+export interface InterfaceForEmployees {
+    id?: number;
+    name: string;
+    surname: string;
+    gender: string;
+    idPosition: number;
+    date: string;
+    age: number;
+}
